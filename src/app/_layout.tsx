6ffd9d279ad72c256f39e79c580df0b3a3c87d0a
Koki Ukai <kooki.ukai100@icloud.com>
2024-04-14 11:43:38 +0900

    Create use-theme-config hook

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,13 @@
 import '../../global.css';
 
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
+import { ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useThemeConfig } from '@/hooks/use-theme-config';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,14 +47,17 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <Providers>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
+    </Providers>
   );
+}
+
+function Providers({ children }: { children: React.ReactNode }) {
+  const theme = useThemeConfig();
+  return <ThemeProvider value={theme}>{children}</ThemeProvider>;
 }
