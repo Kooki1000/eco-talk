ed4a169f48dcba1bc5010b755ebd1fc96bdb9a2e
Koki Ukai <kooki.ukai100@icloud.com>
2024-04-14 11:28:47 +0900

    Test internationalization on modal

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/edit-screen-info';
 import { Text, View } from '@/components/themed';
+import { translate, useSelectedLanguage } from '@/i18n';
+import type { Language } from '@/i18n/resources';
 
 export default function ModalScreen() {
+  const { setLanguage } = useSelectedLanguage();
+  const onSelect = useCallback(
+    (lang: Language) => setLanguage(lang),
+    [setLanguage]
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>{translate('language.title')}</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/modal.tsx" />
+
+      <Text onPress={() => onSelect('en')}>English</Text>
+      <Text onPress={() => onSelect('jp')}>Japanese</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
