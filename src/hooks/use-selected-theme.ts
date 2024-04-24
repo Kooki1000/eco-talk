a45124aaeef8db86387d050a5a87eafc51d2bb77
Koki Ukai <kooki.ukai100@icloud.com>
2024-04-25 08:32:06 +0900

    Update React imports

--- src/hooks/use-selected-theme.ts
+++ src/hooks/use-selected-theme.ts
@@ -3,7 +3,7 @@
 // Original code by OBytes (https://github.com/obytes), licensed under the MIT License.
 
 import { colorScheme, useColorScheme } from 'nativewind';
-import React from 'react';
+import { useCallback } from 'react';
 import { useMMKVString } from 'react-native-mmkv';
 
 import { storage } from '@/lib/storage';
@@ -14,7 +14,7 @@ export type ColorSchemeType = 'light' | 'dark' | 'system';
 /**
  * This hook should only be used while selecting the theme.
  * This hook will return the selected theme which is stored in MMKV.
- * selectedTheme should be one of the following values 'light', 'dark' or 'system'.
+ * selectedTheme should be one of the following values `light`, `dark` or `system`.
  * Don't use this hook if you want to use it to style your component based on the theme use useColorScheme from nativewind instead.
  */
 
@@ -22,7 +22,7 @@ export const useSelectedTheme = () => {
   const { colorScheme: _color, setColorScheme } = useColorScheme();
   const [theme, _setTheme] = useMMKVString(SELECTED_THEME, storage);
 
-  const setSelectedTheme = React.useCallback(
+  const setSelectedTheme = useCallback(
     (t: ColorSchemeType) => {
       setColorScheme(t);
       _setTheme(t);
