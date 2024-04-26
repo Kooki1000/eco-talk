89ea5a79b7113051f3df675bc1eea0f78dc5f956
Koki Ukai <kooki.ukai100@icloud.com>
2024-04-26 16:00:31 +0900

    Revert "Update React imports"
    This reverts commit a45124aaeef8db86387d050a5a87eafc51d2bb77.

--- src/hooks/use-client-only-value.web.ts
+++ src/hooks/use-client-only-value.web.ts
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 
 // `useEffect` is not invoked during server rendering, meaning
 // we can use this to determine if we're on the server or not.
 export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
-  const [value, setValue] = useState<S | C>(server);
-  useEffect(() => {
+  const [value, setValue] = React.useState<S | C>(server);
+  React.useEffect(() => {
     setValue(client);
   }, [client]);
 
