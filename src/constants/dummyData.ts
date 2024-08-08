556e47d18a77cca145307119bb36c7d6d53f627f
Koki Ukai <kooki.ukai100@icloud.com>
2024-08-08 12:51:39 +0900

    Update styling for flatlist

--- src/constants/dummyData.ts
+++ src/constants/dummyData.ts
@@ -3,14 +3,23 @@ import type { Language } from '@/i18n/resources';
 export const loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id malesuada mi, ut tempus diam. Morbi a purus sit amet neque gravida pellentesque. Morbi rhoncus sed leo sed dictum. Nam vulputate consectetur neque, ut dictum justo rhoncus et. Donec pellentesque sodales arcu eget sagittis. Donec faucibus congue pulvinar. Donec est mauris, molestie ac dolor id, sollicitudin iaculis diam. Vivamus sodales blandit auctor.`;
 
 type PostDataType = {
+  id: number;
   variant: 'red' | 'orange' | 'green' | 'blue' | 'purple';
   langCode: Language;
 };
 
 export const postsData: PostDataType[] = [
-  { variant: 'red', langCode: 'en' },
-  { variant: 'orange', langCode: 'en' },
-  { variant: 'green', langCode: 'en' },
-  { variant: 'blue', langCode: 'en' },
-  { variant: 'purple', langCode: 'en' },
+  { id: 1, variant: 'red', langCode: 'en' },
+  { id: 2, variant: 'orange', langCode: 'jp' },
+  { id: 3, variant: 'green', langCode: 'en' },
+  { id: 4, variant: 'blue', langCode: 'jp' },
+  { id: 5, variant: 'purple', langCode: 'en' },
+  { id: 6, variant: 'green', langCode: 'en' },
+  { id: 7, variant: 'purple', langCode: 'jp' },
+  { id: 8, variant: 'blue', langCode: 'en' },
+  { id: 9, variant: 'orange', langCode: 'en' },
+  { id: 10, variant: 'red', langCode: 'jp' },
+  { id: 11, variant: 'green', langCode: 'en' },
+  { id: 12, variant: 'blue', langCode: 'jp' },
+  { id: 13, variant: 'purple', langCode: 'en' },
 ];
