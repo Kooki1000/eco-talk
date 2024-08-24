d82f7b6ac3afc16dbe0322d6b7271eeba31eb4b0
Koki Ukai <kooki.ukai100@icloud.com>
2024-08-24 17:18:58 +0900

    chore: Update address component and translations, add address select feature

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,12 +6,16 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Calendar } from '@/components/calendar';
 import HomeHeader from '@/components/headers/homeHeader';
 import UserInfoHeader from '@/components/headers/userInfoHeader';
+import { Text } from '@/components/obytes';
+import { AddressSelect } from '@/components/settings/AddressSelect';
 import { dayData } from '@/constants/dummyData';
+import { useSelectedAddress } from '@/hooks/use-selected-address';
 
 dayjs.extend(localizedFormat);
 
 export default function HomeScreen() {
   const insets = useSafeAreaInsets();
+  const { selectedAddress } = useSelectedAddress();
 
   return (
     <View
@@ -23,15 +27,26 @@ export default function HomeScreen() {
     >
       <UserInfoHeader />
 
-      <View style={{ width: '80%' }}>
-        <FlatList
-          data={dayData}
-          renderItem={({ item }) => <Calendar data={item} />}
-          ListHeaderComponent={<HomeHeader />}
-          ListFooterComponent={<View style={{ height: 128 }} />}
-          className="mx-auto w-full"
-        />
-      </View>
+      {selectedAddress ? (
+        <View style={{ width: '80%' }}>
+          <FlatList
+            data={dayData}
+            renderItem={({ item }) => <Calendar data={item} />}
+            ListHeaderComponent={<HomeHeader />}
+            ListFooterComponent={<View style={{ height: 128 }} />}
+            className="mx-auto w-full"
+          />
+        </View>
+      ) : (
+        <>
+          <HomeHeader />
+
+          <View className="mt-12 items-center justify-center rounded-md bg-red-200 p-16 dark:bg-red-500">
+            <Text tx="home.setAddress" className="mb-8 text-xl" />
+            <AddressSelect />
+          </View>
+        </>
+      )}
     </View>
   );
 }
