17e74ddf85668a1827b7fed927e45d61b73d4142
Koki Ukai <kooki.ukai100@icloud.com>
2024-09-01 21:42:15 +0900

    refactor: Add ActivityIndicator to PostComponent and ReplyComponent

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -5,11 +5,10 @@ import utc from 'dayjs/plugin/utc';
 import { CircleUserRound } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 import { memo, useMemo, useState } from 'react';
-import { Pressable, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Pressable, StyleSheet, View } from 'react-native';
 import type { VariantProps } from 'tailwind-variants';
 import { tv } from 'tailwind-variants';
 
-import { loremText } from '@/constants/dummyData';
 import { useAuth } from '@/providers/auth-provider';
 import type { DetailedPost, VariantColor } from '@/types/types';
 
@@ -137,7 +136,9 @@ const PostComponent = ({
             borderColor: isDark ? '#e5e7eb' : '#9ca3af',
           }}
         >
-          <Text className="my-4 px-4">{loremText}</Text>
+          <View className="mt-6">
+            <ActivityIndicator />
+          </View>
         </View>
       )}
 
